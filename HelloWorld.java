7b5b1ba23566066b20cace8e1df24d14e0ce42ac
kamleshpandey25k <kamleshpandey25k@gmail.com>
2024-11-08 13:57:43 +0530

    Hello-World-Program

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+
+package Com.java.basic;
+
+/**
+ *
+ * @author kamlesh
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello"); 
+        System.out.print("World\n");
+        System.out.println("\"nnHanuman ji\"");
+    }
+    
+}
